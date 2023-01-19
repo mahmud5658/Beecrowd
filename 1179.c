9a92383bfaf87355fd0b02116fc23a2dbc84b7e8
Abdullah Al Mahmud <104672914+mahmud5658@users.noreply.github.com>
2023-01-19 14:13:09 +0600

    bee-1179 Array Fill IV

---
+++ 1179.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int n,par[5],impar[5],i;
+
+    for(i=0;i<15;i++) {
+
+        scanf("%d",&n);
+
+        if(n%2==0) {
+
+            par[i] = n;
+        }
+
+    }
+}
