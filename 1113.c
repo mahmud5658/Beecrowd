1852561bc1da99882b51b7f1bee104771c8a6276
Abdullah Al Mahmud <104672914+mahmud5658@users.noreply.github.com>
2023-01-19 13:50:29 +0600

    bee-1113 Ascending and Descending

---
+++ 1113.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a,b;
+
+    for(;;) {
+
+        scanf("%d %d",&a,&b);
+
+        if(a>b) 
+
+        printf("Decrescente\n");
+        
+        else if(a<b)
+
+        printf("Crescente\n");
+
+        else if(a==b) 
+
+        break;
+    }
+
+    return 0;
+}
